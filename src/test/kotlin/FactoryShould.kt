9da40fb988d2f9f2f423639d67342036b764ddd2
fdelcaz <fernando.delcaz@amadeus.com>
2025-03-19 17:51:46 +0100

    refactor: move robot tests to the factory tests as the are testing boundaries within the factory using robot as a collaborator

--- src/test/kotlin/FactoryShould.kt
+++ src/test/kotlin/FactoryShould.kt
@@ -1,8 +1,17 @@
 import domain.Factory
+import domain.Heading
 import domain.Position
+import domain.Robot
 import domain.exceptions.OutsideOfTheFactoryBoundariesException
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
+import kotlin.test.assertNull
+
+private const val FACTORY_FLOOR_ROWS = 5
+private const val FACTORY_FLOOR_COLUMNS = 5
 
 class FactoryShould {
 
@@ -10,6 +19,40 @@ class FactoryShould {
     fun raiseAnExceptionIfAskingOutsideTheGrid(){
          val factory = Factory(3,3);
          assertFailsWith<OutsideOfTheFactoryBoundariesException> { factory.whatsIn(Position(3, 4)) }
+    }
 
+    @Test
+    fun returnNullIfAskingForAnEmptySlot(){
+        val factory = Factory(3,3);
+        assertNull(factory.whatsIn(Position(0,0)));
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "5, 6",
+        "6, 5",
+        "-1, 5",
+        "5, -1",
+    )
+    fun raiseAnExceptionIfPlacedOutsideTheGrid(positionX: Int, positionY: Int) {
+
+        val factory = Factory(FACTORY_FLOOR_ROWS, FACTORY_FLOOR_COLUMNS)
+
+        assertFailsWith<OutsideOfTheFactoryBoundariesException>("Robot placed outside grid boundaries!") {
+            factory.place(Robot(Position(positionX, positionY), Heading.NORTH));
+        }
+    }
+
+    @Test
+    fun beAbleToBePlacedInAnEmptySlotOnTheFloor() {
+
+        val factory = Factory(FACTORY_FLOOR_ROWS, FACTORY_FLOOR_COLUMNS)
+        val position = Position(0, 0)
+        val myRobot = Robot(position, Heading.NORTH)
+
+        factory.place(myRobot);
+
+        val expectedRobot = factory.whatsIn(position);
+        assertEquals(expectedRobot, myRobot, "Robot is not place where it should be")
     }
 }
--- src/test/kotlin/RobotShould.kt
+++ src/test/kotlin/RobotShould.kt
@@ -1,42 +1,5 @@
-import domain.*
-import domain.exceptions.OutsideOfTheFactoryBoundariesException
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.CsvSource
-import kotlin.test.Test
-import kotlin.test.assertEquals
-import kotlin.test.assertFailsWith
-
-private const val FACTORY_FLOOR_ROWS = 5
-private const val FACTORY_FLOOR_COLUMNS = 5
 
 class RobotShould {
 
-    @ParameterizedTest
-    @CsvSource(
-        "5, 6",
-        "6, 5",
-        "-1, 5",
-        "5, -1",
-    )
-    fun raiseAnExceptionIfPlacedOutsideTheGrid(positionX: Int, positionY: Int) {
 
-        val factory = Factory(FACTORY_FLOOR_ROWS, FACTORY_FLOOR_COLUMNS)
-
-        assertFailsWith<OutsideOfTheFactoryBoundariesException>("Robot placed outside grid boundaries!") {
-            factory.place(Robot(Position(positionX, positionY), Heading.NORTH));
-        }
-    }
-
-    @Test
-    fun beAbleToBePlacedInAnEmptySlotOnTheFloor() {
-
-        val factory = Factory(FACTORY_FLOOR_ROWS, FACTORY_FLOOR_COLUMNS)
-        val position = Position(0, 0)
-        val myRobot = Robot(position, Heading.NORTH)
-
-        factory.place(myRobot);
-
-        val expectedRobot = factory.whatsIn(position);
-        assertEquals(expectedRobot, myRobot, "Robot is not place where it should be")
-    }
 }
