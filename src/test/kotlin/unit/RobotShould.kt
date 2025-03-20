8067df93305cd9ef1962124ea165229d01227970
fdelcaz <fernando.delcaz@amadeus.com>
2025-03-20 10:00:40 +0100

    test: parameterize rotations

--- src/test/kotlin/unit/RobotShould.kt
+++ src/test/kotlin/unit/RobotShould.kt
@@ -1,42 +1,47 @@
 package unit
 
 import domain.*
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import kotlin.test.assertEquals
 
 class RobotShould {
 
-     @Test
-     fun beAssignedToOneFactory(){
-         val factory = Factory(5, 5);
-         val robot = Robot(Status(Position(0,0), Heading.NORTH), factory);
-
-         assertEquals(robot, factory.whatsIn(robot.status.position));
-         assertEquals(robot.factory, factory);
-     }
-
-    @Test
-    fun rotateRight(){
-        val factory = Factory(5, 5);
-        val robot = Robot(Status(Position(0,0), Heading.NORTH), factory);
-        val expectedStatus = Status(Position(0,0), Heading.EAST);
-
-        val instruction = Rotation(Direction.RIGHT);
-        robot.updateStatus(instruction)
-
-        assertEquals(robot.status, expectedStatus);
+    private lateinit var factory: Factory
+    private lateinit var robot: Robot
+    @BeforeEach
+    fun setup() {
+        factory = Factory(5, 5)
+        robot = Robot(Status(Position(0, 0), Heading.NORTH), factory)
     }
 
     @Test
-    fun rotateLeft(){
-        val factory = Factory(5, 5);
-        val robot = Robot(Status(Position(0,0), Heading.NORTH), factory);
-        val expectedStatus = Status(Position(0,0), Heading.WEST);
+    fun beAssignedToOneFactory(){
+        assertEquals(robot, factory.whatsIn(robot.status.position));
+        assertEquals(robot.factory, factory);
+    }
 
-        val instruction = Rotation(Direction.LEFT);
+    @ParameterizedTest
+    @CsvSource(
+        "NORTH, RIGHT, EAST",
+        "EAST, RIGHT, SOUTH",
+        "SOUTH, RIGHT, WEST",
+        "WEST, RIGHT, NORTH",
+        "NORTH, LEFT, WEST",
+        "WEST, LEFT, SOUTH",
+        "SOUTH, LEFT, EAST",
+        "EAST, LEFT, NORTH",
+    )
+    fun rotateTest(initialHeading: Heading, rotationDirection: String, expectedHeading: Heading) {
+        robot = Robot(Status(Position(0, 0), initialHeading), factory)
+        val instruction = Rotation(Direction.valueOf(rotationDirection))
+        val expectedStatus = Status(Position(0, 0), expectedHeading)
+
         robot.updateStatus(instruction)
 
-        assertEquals(robot.status, expectedStatus);
+        assertEquals(robot.status, expectedStatus)
     }
 }
 
