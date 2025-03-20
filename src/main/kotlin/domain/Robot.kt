6adbddbfa7c9833d1608adc32bf2e4391c78db6a
fdelcaz <fernando.delcaz@amadeus.com>
2025-03-20 09:38:19 +0100

    chore: rename to beAssignedToOneFactory

--- src/main/kotlin/domain/Robot.kt
+++ src/main/kotlin/domain/Robot.kt
@@ -4,5 +4,4 @@ data class Robot(val status: Status, var factory: Factory) {
     init {
         factory.place(this)
     }
-
 }
--- src/test/kotlin/unit/RobotShould.kt
+++ src/test/kotlin/unit/RobotShould.kt
@@ -7,11 +7,13 @@ import kotlin.test.assertEquals
 class RobotShould {
 
      @Test
-     fun beAbleToBeAssignedToOneFactory(){
+     fun beAssignedToOneFactory(){
          val factory = Factory(5, 5);
          val robot = Robot(Status(Position(0,0), Heading.NORTH), factory);
 
          assertEquals(robot, factory.whatsIn(robot.status.position));
          assertEquals(robot.factory, factory);
      }
+
+
 }
