aec4b317801e52f3f586d6ccdb16fd815597bf6c
fdelcaz <fernando.delcaz@amadeus.com>
2025-03-20 09:34:01 +0100

    test: beAbleToBeAssignedToOneFactory

--- src/main/kotlin/domain/Robot.kt
+++ src/main/kotlin/domain/Robot.kt
@@ -1,5 +1,11 @@
 package domain
 
 data class Robot(val status: Status) {
+    lateinit var factory: Factory
+
+    fun assignTo(factory: Factory) {
+        this.factory = factory;
+        factory.place(this);
+    }
 
 }
--- src/test/kotlin/unit/RobotShould.kt
+++ src/test/kotlin/unit/RobotShould.kt
@@ -1,6 +1,18 @@
 package unit
 
+import domain.*
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
 class RobotShould {
 
+     @Test
+     fun beAbleToBeAssignedToOneFactory(){
+         val robot = Robot(Status(Position(0,0), Heading.NORTH));
+         val factory = Factory(5, 5);
 
+         robot.assignTo(factory);
+         assertEquals(robot, factory.whatsIn(robot.status.position));
+         assertEquals(robot.factory, factory);
+     }
 }
