fbf73de79e29e7b4e03906bb27852627a58b0f02
fdelcaz <fernando.delcaz@amadeus.com>
2025-03-20 16:33:45 +0100

    refactor: move statusparser to the infra layer

---
+++ src/main/kotlin/infrastructure/RobotStatusParser.kt
@@ -0,0 +1,31 @@
+package infrastructure
+
+import domain.Heading
+import domain.InvalidStatusException
+import domain.Position
+import domain.Status
+import java.util.*
+
+class RobotStatusParser {
+    fun parse(initialStatus: String): Status {
+        val statusParts = initialStatus.split(" ");
+        if (statusParts.size != 3) {
+            throw InvalidStatusException("Invalid status format")
+        }
+
+        val x = statusParts[0].toIntOrNull()
+        val y = statusParts[1].toIntOrNull()
+
+        if(x == null || y == null){
+            throw InvalidStatusException("Invalid coordinates")
+        }
+
+        val heading = Heading.values().find { it.char == statusParts[2].uppercase(Locale.getDefault())[0] }
+
+        if (heading == null) {
+            throw InvalidStatusException("Invalid heading")
+        }
+
+        return Status(Position(1, 2), Heading.NORTH);
+    }
+}
--- src/test/kotlin/unit/RobotStatusParserShould.kt
+++ src/test/kotlin/unit/RobotStatusParserShould.kt
@@ -4,6 +4,7 @@ import domain.Heading
 import domain.InvalidStatusException
 import domain.Position
 import domain.Status
+import infrastructure.RobotStatusParser
 import java.util.*
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -53,27 +54,5 @@ class RobotStatusParserShould {
     }
 
 
-    class RobotStatusParser {
-        fun parse(initialStatus: String): Status {
-            val statusParts = initialStatus.split(" ");
-            if (statusParts.size != 3) {
-                throw InvalidStatusException("Invalid status format")
-            }
 
-            val x = statusParts[0].toIntOrNull()
-            val y = statusParts[1].toIntOrNull()
-
-            if(x == null || y == null){
-                throw InvalidStatusException("Invalid coordinates")
-            }
-
-            val heading = Heading.values().find { it.char == statusParts[2].uppercase(Locale.getDefault())[0] }
-
-            if (heading == null) {
-                throw InvalidStatusException("Invalid heading")
-            }
-
-            return Status(Position(1, 2), Heading.NORTH);
-        }
-    }
 }
