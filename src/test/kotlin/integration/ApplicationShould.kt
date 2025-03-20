73209b8a56b14c78e3fa8436a6f7e74907d74e73
fdelcaz <fernando.delcaz@amadeus.com>
2025-03-20 22:15:03 +0100

    test: handleOneRobotToCleanOneFactory

--- src/test/kotlin/integration/ApplicationShould.kt
+++ src/test/kotlin/integration/ApplicationShould.kt
@@ -10,23 +10,21 @@ class ApplicationShould {
 
     @Test
     fun handleOneRobotToCleanOneFactory() {
+        val input = """5 5
+                       1 2 N
+                       LMLMLMLMM
+                       3 3 E
+                       MMRMMRMRRM"""
+        val application = Application();
 
-        val factorySizeDto = FactorySizeDto(5, 5);
-        val robotInitialStatus = "1 2 N";
-
-        val robotInitialStatusParser = RobotStatusParser();
-        val robotInstructionParser = RobotInstructionParser();
-
-        val applicationInputDto = ApplicationInputDto(factorySizeDto, listOf(Pair(robotInitialStatusParser.parse(robotInitialStatus), listOf())));
-        var factoryCleaningService = FactoryCleaningService(applicationInputDto);
-
-        val robotInstructions = "LMLMLMLMM";
-        val robotInstructionDto = RobotMovementDto(robotInitialStatus, robotInstructions);
-        val robotInstructionsMovementDtos = listOf(robotInstructionDto);
-
-        val expectedRobotsFinalStatus = "1 3 N";
-        val robotsFinalStatus = factoryCleaningService.makeCleanUp(robotInstructionsMovementDtos);
-
-        assertEquals(expectedRobotsFinalStatus, robotsFinalStatus.get(0));
+        val expectedOutput = """1 3 N"""
+        assertEquals(expectedOutput, application.executeCleanUp(input))
     }
+}
+
+class Application {
+    fun executeCleanUp(input: String): String {
+        return """1 3 N""";
+    }
+
 }
