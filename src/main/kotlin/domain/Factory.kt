fcc37ac4f0d9ae31cfb68266d13c8d41b856af4f
fdelcaz <fernando.delcaz@amadeus.com>
2025-03-20 23:31:07 +0100

    refactor: extract rollback function

--- src/main/kotlin/domain/Factory.kt
+++ src/main/kotlin/domain/Factory.kt
@@ -16,20 +16,27 @@ class Factory(private val rows: Int, private val columns: Int) {
 
         try {
             factoryFloor.clearTile(robot)
+
             robot.status = desiredStatus
             updateTileContent(robot)
+
             return robot.status
         } catch (exception: OutsideOfTheFactoryBoundariesException) {
+
             print("Robot cannot go out of the factory grid boundaries!")
-            robot.status = initialRobotStatus
-            updateTileContent(robot);
-            return initialRobotStatus
+            return rollBack(robot, initialRobotStatus)
+
         } catch (exception: TileAlreadyOccupiedException) {
+
             print("Tile was already occupied! Skipping movement")
-            robot.status = initialRobotStatus
-            updateTileContent(robot);
-            return initialRobotStatus
+            return rollBack(robot, initialRobotStatus)
         }
+    }
+
+    private fun rollBack(robot: Robot, initialRobotStatus: Status): Status {
+        robot.status = initialRobotStatus
+        updateTileContent(robot);
+        return initialRobotStatus
     }
 
     private fun updateTileContent(robot: Robot) {
@@ -44,29 +51,4 @@ class Factory(private val rows: Int, private val columns: Int) {
     }
 }
 
-class FactoryFloor(private val rows: Int, private val columns: Int) {
 
-    private val occupiedPositions: MutableMap<Position, Robot> = mutableMapOf()
-
-    fun clearTile(robot: Robot) {
-        val currentRobot = occupiedPositions[robot.status.position]
-        if (currentRobot != null && currentRobot !== robot) {
-            throw TileAlreadyOccupiedException("The tile is occupied by another robot.")
-        }
-        occupiedPositions.remove(robot.status.position)
-    }
-    fun occupyTile(robot: Robot) {
-        occupiedPositions[robot.status.position] = robot
-    }
-
-    fun isTileOccupied(position: Position): Boolean {
-        if (isOutOfBoundaries(position)) {
-            throw OutsideOfTheFactoryBoundariesException("You can’t query a position outside the factory grid boundaries")
-        }
-
-        return occupiedPositions.containsKey(position)
-    }
-    private fun isOutOfBoundaries(position: Position): Boolean {
-        return position.x < 0 || position.x >= columns || position.y < 0 || position.y >= rows
-    }
-}
---
+++ src/main/kotlin/domain/FactoryFloor.kt
@@ -0,0 +1,31 @@
+package domain
+
+import domain.exceptions.OutsideOfTheFactoryBoundariesException
+import domain.exceptions.TileAlreadyOccupiedException
+
+class FactoryFloor(private val rows: Int, private val columns: Int) {
+
+    private val occupiedPositions: MutableMap<Position, Robot> = mutableMapOf()
+
+    fun clearTile(robot: Robot) {
+        val currentRobot = occupiedPositions[robot.status.position]
+        if (currentRobot != null && currentRobot !== robot) {
+            throw TileAlreadyOccupiedException("The tile is occupied by another robot.")
+        }
+        occupiedPositions.remove(robot.status.position)
+    }
+    fun occupyTile(robot: Robot) {
+        occupiedPositions[robot.status.position] = robot
+    }
+
+    fun isTileOccupied(position: Position): Boolean {
+        if (isOutOfBoundaries(position)) {
+            throw OutsideOfTheFactoryBoundariesException("You can’t query a position outside the factory grid boundaries")
+        }
+
+        return occupiedPositions.containsKey(position)
+    }
+    private fun isOutOfBoundaries(position: Position): Boolean {
+        return position.x < 0 || position.x >= columns || position.y < 0 || position.y >= rows
+    }
+}
