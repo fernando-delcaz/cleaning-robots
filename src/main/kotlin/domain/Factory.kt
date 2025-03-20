a323328c3d549721c1042d8ea54a0cc0638cb351
fdelcaz <fernando.delcaz@amadeus.com>
2025-03-20 11:03:14 +0100

    refactor: remove unneeded exception

--- src/main/kotlin/domain/Factory.kt
+++ src/main/kotlin/domain/Factory.kt
@@ -8,18 +8,14 @@ class Factory(private val rows: Int, private val columns: Int) {
 
     internal fun place(robot: Robot) {
         try {
-            factoryFloor[robot.status.position.x][robot.status.position.y] = robot;
+            factoryFloor[robot.status.position.x][robot.status.position.y] = robot
         } catch (exception: ArrayIndexOutOfBoundsException) {
             throw OutsideOfTheFactoryBoundariesException("Robot placed outside grid boundaries!")
         }
     }
 
     private fun emptyTile(status: Status) {
-        try {
-            factoryFloor[status.position.y][status.position.x] = null;
-        } catch (exception: ArrayIndexOutOfBoundsException) {
-            throw OutsideOfTheFactoryBoundariesException("Robot placed outside grid boundaries!")
-        }
+        factoryFloor[status.position.y][status.position.x] = null
     }
 
     fun whatsIn(position: Position): Robot? {
@@ -29,17 +25,18 @@ class Factory(private val rows: Int, private val columns: Int) {
         return factoryFloor[position.x][position.y]
     }
 
-    internal fun updateRobotStatus(robot: Robot, desiredStatus: Status) : Status{
-        try{
-            val updatedRobot = robot.copy(status = desiredStatus);
+    internal fun updateRobotStatus(robot: Robot, desiredStatus: Status): Status {
+        try {
+            val updatedRobot = robot.copy(status = desiredStatus)
             place(updatedRobot)
-            emptyTile(robot.status);
-            return updatedRobot.status;
-        }catch (exception: ArrayIndexOutOfBoundsException){
+            emptyTile(robot.status)
+            return updatedRobot.status
+        } catch (exception: ArrayIndexOutOfBoundsException) {
             print("Robot cannot be moved!")
-            return robot.status;
+            return robot.status
         }
     }
 
-    private fun isOutOfTheFactoryBoundaries(position: Position) = position.x < 0 || position.x > rows || position.y < 0 || position.y > columns
+    private fun isOutOfTheFactoryBoundaries(position: Position) =
+        position.x < 0 || position.x > rows || position.y < 0 || position.y > columns
 }
